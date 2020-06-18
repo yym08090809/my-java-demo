c2ce374ea74cac019774c08b5cf063ef9c890cd4
yym08090809 <1217393203@qq.com>
2020-06-19 00:21:51 +0800

    力扣、蓝桥杯、牛客刷题代码

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # my-java-demo
 刷题代码
+
---
+++ src/main/java/com/lcx/leetCode/Demo20.java
@@ -0,0 +1,34 @@
+package com.lcx.leetCode;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Demo20 {
+    private HashMap<Character, Character> map;
+
+    public Demo20() {
+        this.map = new HashMap<>();
+        map.put(')', '(');
+        map.put('{', '}');
+        map.put('[', ']');
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (this.map.containsKey(c)) {
+                //获取栈中的栈顶元素，若栈顶元素为空则设置为‘#’
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                if (topElement != this.map.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
