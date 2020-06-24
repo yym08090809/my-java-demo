fc8a6b0f4a3e9df081319f4bdcfb82d500ce1e8b
yym08090809 <1217393203@qq.com>
2020-06-25 00:41:22 +0800

    力扣、蓝桥杯、牛客刷题代码

---
+++ src/main/java/com/lcx/leetCode/ThreeNumsAdd.java
@@ -0,0 +1,29 @@
+package com.lcx.leetCode;
+
+import java.util.Arrays;
+
+public class ThreeNumsAdd {
+    public int threeSumClosest(int[] nums, int target) {
+//        if(nums.length < 3 || nums.length > Math.pow(10,3) || target < Math.pow(-10,4) || target > Math.pow(10,4)){
+//            return 0;
+//        }else {
+//            for (int i = 0; i < nums.length; i++) {
+//                if(nums[i] < Math.pow(-10,3) || nums[i] > Math.pow(10,3)){
+//                    return 0;
+//                }else {
+//                    Arrays.sort(nums);
+//                    int n = nums.length;
+//                    int a,b,c;//三个数
+//                    int p,q;//双指针
+//                    for (int j = 0; j < n; j++) {
+//                        a = nums[j];
+//                        for (int k = j; k < nums.length; k++,) {
+//
+//                        }
+//                    }
+//                }
+//            }
+//        }
+        return 0;
+    }
+}
---
+++ src/main/java/com/lcx/niuke/Rabbit.java
@@ -0,0 +1,30 @@
+package com.lcx.niuke;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Rabbit {
+    /**
+     * 统计出兔子总数。
+     *
+     * @param monthCount 第几个月
+     * @return 兔子总数
+     */
+    public static int getTotalCount(int monthCount) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int f1=1,f2=1;
+            map.put(1,1);
+            map.put(2,1);
+        for (int i = 3; i <= monthCount; i++) {
+            map.put(i,map.get(i-1)+map.get(i-2));
+        }
+        return map.get(monthCount);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int mountCount = scanner.nextInt();
+        System.out.println(getTotalCount(mountCount));
+    }
+}
